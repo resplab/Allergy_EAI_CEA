fb62254d856a45caf414e9be4b7811202ba5a978
Kate Johnson <katemarjohnson@gmail.com>
2023-07-20 20:12:46 -0700

    Code cleaning and adding additional data from Markov trace

--- Allergy_EAI_model.R
+++ Allergy_EAI_model.R
@@ -4,34 +4,26 @@ library(diagram)
 library(ggplot2)
 
 
-
 # import the Canada 2020 life table with mortality value in all ages (both sex)
 
 life_table<-read.csv('./life_table_CAN_2020.csv')
 
 #remove row 111 from life_table (age older than 111 )
-life_table<- life_table[-111,]
-life_table_seperate_unit<-life_table %>%
-  separate(Age.group, into = c("Age", "Unit"), sep = " ")
+life_table <- life_table %>% 
+                filter(Age.group != "110 years and over") %>% 
+                separate(Age.group, into = c("Age", "Unit"), sep = " ") %>% 
+                select(Age, VALUE) %>% 
+                mutate(Age=as.numeric(Age), fatality_daily = rescale_prob(p = VALUE, from = 365 ))
  
-#clean lifetable
-life_table_clean <- select (life_table_seperate_unit, Age, VALUE)
-life_table_clean<-as.data.frame(life_table_clean)
-life_table_clean$Age<-as.numeric((life_table_clean$Age))
-
-#calculate daily death probability
-life_table_clean<-life_table_clean %>% mutate(fatality_daily = rescale_prob(p = VALUE, from = 365 ))
- life_table_clean
-
 #build age parameter,
 par_allerg <-define_parameters(
-  age_initial = 1,
-  age = floor(age_initial + markov_cycle/365)
+                  age_initial = 1,
+                  age = floor(age_initial + markov_cycle/365)
 )
 
 
-#_ww stands for the parameter for watch and wait
-#_ED stands for parameter for transfer to ED, 
+#_ww: watch and wait scenario
+#_ED: transfer to ED scenario 
 #ns: non severe
 #sw: severe watch and wait,
 #sED: severe transfer to ED,
@@ -44,47 +36,51 @@ par_allerg <-define_parameters(
 
 par_allerg<-modify(
 par_allerg,
-p_ns_ar = rescale_prob( p = 0.017,from = 365), # transition from non-severe reaction to food allergy remission
-p_ns_sw_ww = rescale_prob( p =0.074820,from = 365), # watch and wait matrix-transition from non-severe reaction to severe reaction watch and wait
-p_ns_sED_ww = rescale_prob(p = 0.01218, from = 365), # watch and wait matrix-transition from non-severe reaction to severe reaction transfer to ED 
-p_sw_faf = rescale_prob(p = 0.00002, from = 365), # Transition from watch and wait to food allergy fatality
+p_ns_ar = rescale_prob(p = 0.017, from = 365), # transition from non-severe reaction to food allergy remission
+p_ns_sw_ww = rescale_prob(p =0.074820, from = 365), # non-severe reaction to severe reaction for watch and wait
+p_ns_sED_ww = rescale_prob(p = 0.01218, from = 365), # non-severe reaction to severe reaction transfer to ED for watch and wait
+p_sw_faf = rescale_prob(p = 0.00002, from = 365), # Watch and wait to food allergy fatality
 p_sED_sh = 0.12, #transition from ED to hospitalization 
-p_sED_faf = rescale_prob(p=0.000002,from = 365), #transition from ED to food allergy fatality
-p_ns_sED_ED= rescale_prob( p = 0.087, from = 365), #ED transfer matrix: transition from non-severe to ED
-p_sh_faf= 0.0045,#transition from hospitalization to food allergy fatality
-acm = look_up(data = life_table3, Age = age, value = "fatality_daily"), #daily based all-case mortality
-dr=rescale_prob( p =0.015, from = 365)
+p_sED_faf = rescale_prob(p = 0.000002, from = 365), # transition from ED to food allergy fatality
+p_ns_sED_ED= rescale_prob(p = 0.087, from = 365), # transition from non-severe to ED
+p_sh_faf= 0.0045,# transition from hospitalization to food allergy fatality
+acm = look_up(data = life_table, Age = age, value = "fatality_daily"), #daily all-cause mortality
+dr=rescale_prob( p=0.015, from = 365)
   )
 
 
 #transition matrix for watch and wait
 Transition_watch <- define_transition(
   state_names = c("state_ar","state_ns", "state_sw", "state_sED", "state_sh", "state_faf","state_acm"),
-  C,0,0,0,0,0,acm,
-  p_ns_ar,C,p_ns_sw_ww,p_ns_sED_ww,0,0,acm,
-  0,C,0,0,0,p_sw_faf,acm,
-  0,C,0,0,p_sED_sh,p_sED_faf,acm,
-  0,C,0,0,0,p_sh_faf,acm,
-  0,0,0,0,0,1,0,
-  0,0,0,0,0,0,1
+  C,          0,     0,          0,           0,        0,         acm,
+  p_ns_ar,    C,     p_ns_sw_ww, p_ns_sED_ww, 0,        0,         acm,
+  0,          C,     0,          0,           0,        p_sw_faf,  acm,
+  0,          C,     0,          0,           p_sED_sh, p_sED_faf, acm,
+  0,          C,     0,          0,           0,        p_sh_faf,  acm,
+  0,          0,     0,          0,           0,        1,           0,
+  0,          0,     0,          0,           0,        0,           1
 )
 
-p_sED_faf
-p_sw_faf
+Transition_watch
+plot(Transition_watch)
 
 #transition matrix for ED transfer 
 Transition_ED <- define_transition(
   state_names = c("state_ar","state_ns", "state_sw", "state_sED", "state_sh", "state_faf","state_acm"),
-  C,0,0,0,0,0,acm,
-  p_ns_ar,C,0,p_ns_sED_ED,0,0,acm,
-  0,0,0,0,0,1,0,
-  0,C,0,0,p_sED_sh,p_sED_faf,acm,
-  0,C,0,0,0,p_sh_faf,acm,
-  0,0,0,0,0,1,0,
-  0,0,0,0,0,0,1
+  C,            0,      0,           0,       0,         0,      acm,
+  p_ns_ar,      C,      0, p_ns_sED_ED,       0,         0,      acm,
+  0,            0,      0,           0,       0,         1,        0,
+  0,            C,      0,           0,p_sED_sh, p_sED_faf,      acm,
+  0,            C,      0,           0,       0,  p_sh_faf,      acm,
+  0,            0,      0,           0,       0,         1,        0,
+  0,            0,      0,           0,       0,         0,        1
 )
 
-#cost for food allergy remission 
+Transition_ED
+plot(Transition_ED)
+
+
+# food allergy remission 
 state_ar<-define_state(
   remission_cost= 513/365,
   medical_cost = 0,
@@ -97,7 +93,7 @@ state_ar<-define_state(
   utility_total = discount(utility, r=dr)
 )
 
-#Cost- non-severe reaction
+# non-severe reaction
 state_ns<-define_state(
   remission_cost= 0,
   medical_cost = 1254/365,
@@ -110,7 +106,7 @@ state_ns<-define_state(
   utility_total = discount(utility, r=dr)
 )
 
-#Cost- severe allergic reaction-watch and waiting 
+#severe allergic reaction-watch and waiting 
 state_sw<-define_state(
   remission_cost= 0,
   medical_cost = 1254/365,
@@ -119,11 +115,11 @@ state_sw<-define_state(
   medical_cost_ED = 0,
   medical_cost_hospitalized =0,
   utility = 0.83,
-  cost_total = discount(medical_cost+treatment_cost,r=dr),
+  cost_total = discount(medical_cost+treatment_cost, r=dr),
   utility_total = discount(utility, r=dr)
 )
 
-#Cost- severe allergic reaction - ED transfer 
+#severe allergic reaction - ED transfer 
 #medical cost =direct cost of food allergy/year+ED medical cost
 state_sED<- define_state(
   remission_cost= 0,
@@ -133,12 +129,11 @@ state_sED<- define_state(
   treatment_cost = 0.8,
   medical_cost_hospitalized =0,
   utility =0.83,
-  cost_total = discount(medical_cost + medical_cost_ED + ambulance_cost+ treatment_cost,r=dr),
+  cost_total = discount(medical_cost + medical_cost_ED + ambulance_cost+ treatment_cost, r=dr),
   utility_total = discount(utility, r=dr)
 )
 
-#cost - severe allergic reaction -hospitalized 
-
+#severe allergic reaction -hospitalized 
 state_sh<-define_state(
   remission_cost= 0,
   medical_cost = 0,
@@ -152,8 +147,7 @@ state_sh<-define_state(
 )
 
 
-#cost -food allergy fatality
-
+#food allergy fatality
 state_faf<-define_state(
   remission_cost= 0,
   medical_cost =0, 
@@ -166,7 +160,7 @@ state_faf<-define_state(
   utility_total = 0
 )
 
-#cost - all-cause mortality
+#all-cause mortality
 state_acm<-define_state(
   remission_cost= 0,
   medical_cost =0, 
@@ -204,26 +198,38 @@ strategy_ED<-define_strategy(
   state_acm = state_acm
 )
 
+time0 <- define_init(state_ar = 0,
+                     state_ns = 10000,
+                     state_sw = 0,
+                     state_sED= 0,
+                     state_sh = 0,
+                     state_faf= 0,
+                     state_acm=0)
 
 allergy_mod<-run_model(
-  parameters = par_allerg,
-  watch_wait = strategy_watch,
-  ED_transfer = strategy_ED,
-  init = c(0,10000,0,0,0,0,0),
-  cycles = 7300,
-  cost = cost_total,
-  effect = utility
+                parameters = par_allerg,
+                watch_wait = strategy_watch,
+                ED_transfer = strategy_ED,
+                init = time0,
+                cycles = 20*365,
+                cost = cost_total,
+                effect = utility
 )
 
-plot(allergy_mod)
-plot(allergy_mod, states = c("state_faf" )) #plot for food allergy fatality
 
+plot(allergy_mod)
+tmp <- get_counts(allergy_mod)
+tmp %>%   
+  group_by(.strategy_names, state_names) %>% 
+  summarise(avg=mean(count), sum=sum(count))
+
+
+plot(allergy_mod, states = c("state_faf" )) #plot for food allergy fatality
 plot(allergy_mod, states = c("state_sh")) #plot for hospitalization 
 plot(allergy_mod, states = c("state_sED")) # plot for 
 plot(allergy_mod, states = c("state_sw"))
 plot(allergy_mod,states = c("state_ns"))
 plot(allergy_mod, states = c("state_ar"))
-
 
 
 
